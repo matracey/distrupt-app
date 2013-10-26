ba310fe165ac58f7a9d1c48f3114fde97bc0a5f8
Brian Gouws <briangouws@gmail.com>
2013-10-26 21:40:01 +0100

    Added base method and functionality for adding a user

--- backend/app/controllers/ApiController.php
+++ backend/app/controllers/ApiController.php
@@ -22,6 +22,25 @@ class ApiController extends \BaseController {
     {
         // command line curl:
         // curl -X POST -d 'd=d' http://local.disrupt.me/api/users
+        // TODO: get lines, and duration, time @ work. sms/email
+
+        $from_line = $_POST['transport']['directions'][0]['line'];
+        $to_line = $_POST['transport']['directions'][1]['line'];
+        $duration = $_POST['transport']['duration'];
+        $time_at_work = $_POST['timeAtWork'];
+
+        $sms_to = array();
+        foreach ($_POST['sms'] as $cell_number) {
+            $sms_to[] = $cell_number;
+        }
+
+        $email_to = array();
+        foreach ($_POST['email'] as $email_address) {
+            $email_to[] = $email_address;
+        }
+
+        // TODO: save this info to DB
+
         die('post users');
     }
 

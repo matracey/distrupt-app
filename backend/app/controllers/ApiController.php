4dd6fecdc1866e7d0e00d3d647afaabc8cd3f1e0
Martin Tracey <matracey@outlook.ie>
2013-10-26 22:31:04 +0000

    Completed the postSms function. Accepts phone numbers (delimited by comma) and message.

--- backend/app/controllers/ApiController.php
+++ backend/app/controllers/ApiController.php
@@ -1,6 +1,11 @@
 <?php
 
 class ApiController extends \BaseController {
+    public $restful = true;
+    private $clickatellAuth = array("session" => null, "retrievalTime" => null);
+    const CLICKATELL_API_ID = '3445709';
+    const CLICKATELL_API_PASS = '9y8NvTuNu';
+    const CLICKATELL_API_USER = 'HACKATHON102';
 
     public function getIndex()
     {
@@ -20,14 +25,69 @@ class ApiController extends \BaseController {
 
     public function postSms()
     {
-        die('smsm');
+        // die('smsm');
         if( !(isset($_POST["phoneNumber"]) and isset($_POST["message"])) )
         {
-            // not sure why but the below doesnt work, will fix later
-//            Response::abort(400, "Bad Request");
+           return Response::make("400: Bad Request", 400);
+        }else{
+            $phoneNumbers = explode(",", $_POST["phoneNumber"]);
+            $message = $_POST["message"];
+
+            // var_dump($phoneNumber, $message, );
+            // exit();
+
+            $this->clickatellAuth();
+
+            foreach ($phoneNumbers as $phoneNumber) {
+                // http://api.clickatell.com/http/sendmsg?session_id=xxxx&to=xxxx&text=xxxx
+                $handler = curl_init();
+                curl_setopt_array($handler, array(
+                    CURLOPT_URL => "http://api.clickatell.com/http/sendmsg",
+                    CURLOPT_POST => TRUE,
+                    CURLOPT_RETURNTRANSFER => TRUE,
+                    CURLOPT_POSTFIELDS => array(
+                        "session_id" => $this->clickatellAuth["session"],
+                        "to" => $phoneNumber,
+                        "text" => $message
+                        )
+                    ));
+
+                $result = curl_exec($handler);
+
+                var_dump($result);
+            }
         }
-        $phoneNumber = $_POST["phoneNumber"];
-        $message = $_POST["message"];
     }
 
+    private function clickatellAuth()
+    {
+        if( !(isset($clickatellAuth["session"]) and isset($clickatellAuth["retrievalTime"])) )
+        {
+            /*
+             * If no session or retieval time is set, or if it has expired, then we must get a new session.
+             */
+
+            // echo "http://api.clickatell.com/http/auth?api_id=".$this::CLICKATELL_API_ID."&user=".$this::CLICKATELL_API_USER."&password=".$this::CLICKATELL_API_PASS;
+
+            $handler = curl_init();
+            curl_setopt_array($handler, array(
+                CURLOPT_URL => "http://api.clickatell.com/http/auth?api_id=".$this::CLICKATELL_API_ID."&user=".$this::CLICKATELL_API_USER."&password=".$this::CLICKATELL_API_PASS,
+                CURLOPT_POST => FALSE,
+                CURLOPT_RETURNTRANSFER => TRUE
+                // CURLOPT_POSTFIELDS => array(
+                    // 'phoneNumber' => "09090"
+                    // 'message' => "Message"
+                    // );
+                ));
+
+            $result = curl_exec($handler);
+
+            if(stristr($result, "OK: ") != FALSE)
+            {
+                $this->clickatellAuth["session"] = substr($result, 4);
+                $this->clickatellAuth["retrievalTime"] = date("d/m/Y H:i:s");
+                // var_dump($this->clickatellAuth);
+            }
+        }
+    }
 }

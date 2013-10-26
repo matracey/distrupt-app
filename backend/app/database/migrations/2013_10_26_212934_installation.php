aeff7a8d616d6190c727637cd156779218a2cd33
Brian Gouws <briangouws@gmail.com>
2013-10-26 22:33:09 +0100

    Added migration sql

---
+++ backend/app/database/migrations/2013_10_26_212934_installation.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class Installation extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+            $sql = "
+                CREATE TABLE IF NOT EXISTS `users` (
+                  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                  `user_fbUUID` varchar(255) NOT NULL,
+                  `user_emails` varchar(255) NOT NULL,
+                  `user_sms` varchar(255) NOT NULL,
+                  `user_time_at_work` varchar(255) NOT NULL,
+                  `user_journey_duration` int(10) unsigned NOT NULL DEFAULT '3600',
+                  `user_line_one` varchar(255) NOT NULL,
+                  `user_line_two` varchar(255) NOT NULL,
+                  PRIMARY KEY (`user_id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;";
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+                $sql = "DROP TABLE users;";
+	}
+
+}

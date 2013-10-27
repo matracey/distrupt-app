077a8283b7946cd9d0c0334c5dc289dae780d5c6
Brian Gouws <briangouws@gmail.com>
2013-10-27 10:03:47 +0000

    Added insert query for users

--- backend/app/database/migrations/2013_10_26_212934_installation.php
+++ backend/app/database/migrations/2013_10_26_212934_installation.php
@@ -14,18 +14,18 @@ class Installation extends Migration {
             $sql = "
 
             CREATE TABLE IF NOT EXISTS `users` (
-              `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `user_fbUUID` varchar(255) NOT NULL,
-              `user_emails` varchar(255) NOT NULL,
-              `user_sms` varchar(255) NOT NULL,
-              `user_time_at_work` varchar(255) NOT NULL,
-              `user_journey_duration` int(10) unsigned NOT NULL DEFAULT '3600',
-              `user_line_one` varchar(255) NOT NULL,
-              `user_line_two` varchar(255) NOT NULL,
-              `user_last_emailed` datetime NOT NULL,
-              `user_lat_sms` datetime NOT NULL,
-              PRIMARY KEY (`user_id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+            `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+            `user_fbUUID` varchar(255) NOT NULL,
+            `user_emails` varchar(255) NOT NULL,
+            `user_sms` varchar(255) NOT NULL,
+            `user_time_at_work` varchar(255) NOT NULL,
+            `user_journey_duration` int(10) unsigned NOT NULL DEFAULT '3600',
+            `user_line_one` varchar(255) NOT NULL,
+            `user_line_two` varchar(255) NOT NULL,
+            `user_last_emailed` datetime NOT NULL,
+            `user_last_sms` datetime NOT NULL,
+            PRIMARY KEY (`user_id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 ";
 
